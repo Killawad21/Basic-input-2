48fe667ee3ff48e4d998744efdfc512e1e68a581
Logan <02logan.pennock@gulllakecs.org>
2020-01-29 10:12:12 -0500

    added +3

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
-
+import java.text.NumberFormat;
+import android.view.View;
+import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
-
+    int quantity = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void three(View view) {
+        quantity = quantity + 3;
+        display(quantity);
+    }
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(
+                R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
     }
 }
