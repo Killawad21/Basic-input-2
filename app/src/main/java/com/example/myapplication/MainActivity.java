5037e34e036dce99139e712ccd7129549294a267
Logan <02logan.pennock@gulllakecs.org>
2020-01-30 09:18:38 -0500

    Add team B

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     int quantity = 0;
+    int quantity2 = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,9 +26,26 @@ public class MainActivity extends AppCompatActivity {
         quantity = quantity + 1;
         display(quantity);
     }
+    public void three2(View view) {
+        quantity2 = quantity2 + 3;
+        display2(quantity2);
+    }
+    public void two2(View view) {
+        quantity2 = quantity2 + 2;
+        display2(quantity2);
+    }
+    public void freeThrow2(View view) {
+        quantity2 = quantity2 + 1;
+        display2(quantity2);
+    }
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(
                 R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+    private void display2(int number) {
+        TextView quantityTextView = (TextView) findViewById(
+                R.id.quantity2_text_view);
         quantityTextView.setText("" + number);
     }
 }
